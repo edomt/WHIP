48903d0a1e8059794538fea6037bc1609ab7f689
edomt <edmat@pm.me>
2019-01-13 21:21:51 +0100

    Update graph

--- make_graph.R
+++ make_graph.R
@@ -12,7 +12,7 @@ g <- ggplot(data = df, aes(x = date, y = dissent, color = group)) +
      x = NULL) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   scale_color_manual(values = c("#FF0000", "#FF8000", "#990000", "#FFEB00", "#0066CC", "#999999", "#FF8080", "#30BFE9")) +
-  theme_grey(base_size = 11) +
+  theme_grey(base_size = 10.5) +
   theme(legend.title = element_blank(), legend.position = "bottom")
 
 ggsave(g, filename = "whip_graph.png", width = 10, height = 5, units = "in")
